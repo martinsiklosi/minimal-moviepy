22ebc717346e5c656eae0f5c6aa3f84cb62de3c0
Martin Siklosi <martin@siklosi.se>
2024-08-20 15:48:40 +0200

    experimental readdition of writing audiofile

--- minimal_moviepy/audio/AudioClip.py
+++ minimal_moviepy/audio/AudioClip.py
@@ -1,7 +1,11 @@
+import os
+
 import numpy as np
 
-from minimal_moviepy.Clip import Clip
-from minimal_moviepy.decorators import requires_duration
+from moviepy.Clip import Clip
+from moviepy.tools import extensions_dict
+from moviepy.decorators import requires_duration
+from moviepy.audio.io.ffmpeg_audiowriter import ffmpeg_audiowrite
 
 
 class AudioClip(Clip):
@@ -134,3 +138,70 @@ class AudioClip(Clip):
         for chunk in self.iter_chunks(chunksize=chunksize,logger=logger):
             maxi = np.maximum(maxi, abs(chunk).max(axis=0)) if stereo else max(maxi, abs(chunk).max())
         return maxi
+    
+    @requires_duration
+    def write_audiofile(self, filename, fps=None, nbytes=2, buffersize=2000,
+                        codec=None, bitrate=None, ffmpeg_params=None,
+                        write_logfile=False, verbose=True):
+        """ Writes an audio file from the AudioClip.
+
+
+        Parameters
+        -----------
+
+        filename
+          Name of the output file
+
+        fps
+          Frames per second. If not set, it will try default to self.fps if
+          already set, otherwise it will default to 44100
+
+        nbytes
+          Sample width (set to 2 for 16-bit sound, 4 for 32-bit sound)
+
+        codec
+          Which audio codec should be used. If None provided, the codec is
+          determined based on the extension of the filename. Choose
+          'pcm_s16le' for 16-bit wav and 'pcm_s32le' for 32-bit wav.
+
+        bitrate
+          Audio bitrate, given as a string like '50k', '500k', '3000k'.
+          Will determine the size and quality of the output file.
+          Note that it mainly an indicative goal, the bitrate won't
+          necessarily be the this in the output file.
+
+        ffmpeg_params
+          Any additional parameters you would like to pass, as a list
+          of terms, like ['-option1', 'value1', '-option2', 'value2']
+
+        write_logfile
+          If true, produces a detailed logfile named filename + '.log'
+          when writing the file
+
+        verbose
+          Boolean indicating whether to print infomation
+          
+        logger
+          Either 'bar' or None or any Proglog logger
+
+        """
+        if not fps:
+            if not self.fps:
+                fps = 44100
+            else:
+                fps = self.fps
+
+        if codec is None:
+            _, ext = os.path.splitext(os.path.basename(filename))
+            try:
+                codec = extensions_dict[ext[1:]]['codec'][0]
+            except KeyError:
+                raise ValueError("MoviePy couldn't find the codec associated "
+                                 "with the filename. Provide the 'codec' "
+                                 "parameter in write_audiofile.")
+
+        return ffmpeg_audiowrite(self, filename, fps, nbytes, buffersize,
+                                 codec=codec, bitrate=bitrate,
+                                 write_logfile=write_logfile, verbose=verbose,
+                                 ffmpeg_params=ffmpeg_params)
+
